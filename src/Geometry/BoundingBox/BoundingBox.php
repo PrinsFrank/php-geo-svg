5ed5719c8a7d6f89baf5b4ea6a4cca531fb860c3
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2021-09-09 21:03:54 +0200

    Move viewbox to bounding box

--- src/Geometry/BoundingBox/BoundingBox.php
+++ src/Geometry/BoundingBox/BoundingBox.php
@@ -17,5 +17,25 @@ class BoundingBox
         if ($this->southWestern->hasElevation() !== $this->northEastern->hasElevation()) {
             throw new InvalidBoundingBoxException('Either both or none of the Positions should have an elevation');
         }
+
+        if ($this->southWestern->longitude > Position::MAX_LONGITUDE) {
+            throw new InvalidBoundingBoxException(
+                'The view box is unnecessarily rotated. Use a minLongitude of "' . (($this->southWestern->longitude + 180) % 360 - 180) . '" instead to achieve the same view.'
+            );
+        }
+
+        if ($this->northEastern->longitude < Position::MIN_LONGITUDE) {
+            throw new InvalidBoundingBoxException(
+                'The view box is unnecessarily rotated. Use a maxLongitude of "' . (($this->northEastern->longitude - 180) % 360 + 180) . '" instead to achieve the same view.'
+            );
+        }
+
+        if ($this->southWestern->latitude < Position::MIN_LATITUDE) {
+            throw new InvalidBoundingBoxException('The minimum Latitude is "' . Position::MIN_LATITUDE . '"');
+        }
+
+        if ($this->northEastern->latitude > Position::MAX_LATITUDE) {
+            throw new InvalidBoundingBoxException('The maximum Latitude is "' . Position::MIN_LATITUDE . '"');
+        }
     }
 }
