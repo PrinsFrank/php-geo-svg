f9c89a62b93d3932cea8331b7ed815fe8d67668a
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2021-09-30 22:41:23 +0200

    Add getter for geometryObjects

--- src/Geometry/GeometryCollection.php
+++ src/Geometry/GeometryCollection.php
@@ -9,12 +9,20 @@ use PrinsFrank\PhpGeoSVG\Geometry\GeometryObject\GeometryObject;
 class GeometryCollection
 {
     /** @var GeometryObject[] */
-    protected array $geometryObjects;
+    protected array $geometryObjects = [];
 
     public function addGeometryObject(GeometryObject $geometryObject): self
     {
         $this->geometryObjects[] = $geometryObject;
 
         return $this;
+    }
+
+    /**
+     * @return GeometryObject[]
+     */
+    public function getGeometryObjects(): array
+    {
+        return $this->geometryObjects;
     }
 }
