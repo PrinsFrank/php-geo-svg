dc4547388d2323f9598920dabea0b849423d07d6
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2021-10-20 21:58:03 +0200

    Add feature test to test completeness and entire features

---
+++ tests/Feature/WorldMapTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PrinsFrank\PhpGeoSVG\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\PhpGeoSVG\Geometry\GeometryCollectionFactory;
+use PrinsFrank\PhpGeoSVG\GeoSVG;
+
+class WorldMapTest extends TestCase
+{
+    private const GEO_JSON_FOLDER = 'vendor/prinsfrank/natural-earth-vector-geojson-only/geojson/';
+
+    public function testGeneratesFromGeoJson(): void
+    {
+        $testName = 'generated-from-geojson';
+
+        (new GeoSVG())
+            ->toFile(
+                GeometryCollectionFactory::createFromGeoJSONFilePath(self::GEO_JSON_FOLDER . '/ne_110m_admin_0_countries.geojson'),
+                __DIR__ . '/actual/' . $testName . '.html'
+            );
+
+        self::assertFileEquals(__DIR__ . '/expected/' . $testName . '.html', __DIR__ . '/actual/' . $testName . '.html');
+    }
+
+    public function testSupportsAllGeoJsonFilesInNaturalEarthVectorSet(): void
+    {
+        $geoJsonFileNames = scandir(dirname(__DIR__, 2) .  '/' .self::GEO_JSON_FOLDER);
+        $geoJsonFileNames = array_diff($geoJsonFileNames, ['.', '..']);
+
+        static::assertCount(162, $geoJsonFileNames);
+        foreach ($geoJsonFileNames as $geoJsonFileName) {
+            (new GeoSVG())
+                ->render(GeometryCollectionFactory::createFromGeoJSONFilePath(self::GEO_JSON_FOLDER . $geoJsonFileName));
+            $this->addToAssertionCount(1);
+        }
+    }
+}
