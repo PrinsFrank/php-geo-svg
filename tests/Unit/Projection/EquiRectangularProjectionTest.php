289cd9386e3a1295afce8a07c341b246e033bcdc
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2021-10-24 18:12:02 +0200

    Add Unit test for EquiRectangular projection

---
+++ tests/Unit/Projection/EquiRectangularProjectionTest.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\PhpGeoSVG\Tests\Unit\Projection;
+
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\PhpGeoSVG\Geometry\Position\Position;
+use PrinsFrank\PhpGeoSVG\Projection\EquiRectangularProjection;
+
+/**
+ * @coversDefaultClass \PrinsFrank\PhpGeoSVG\Projection\EquiRectangularProjection
+ */
+class EquiRectangularProjectionTest extends TestCase
+{
+    /**
+     * @covers ::getX
+     */
+    public function testGetX(): void
+    {
+        static::assertSame(180.0, (new EquiRectangularProjection())->getX(new Position(0, 0)));
+        static::assertSame(0.0, (new EquiRectangularProjection())->getX(new Position(-180, 0)));
+        static::assertSame(360.0, (new EquiRectangularProjection())->getX(new Position(180, 0)));
+    }
+
+    /**
+     * @covers ::getY
+     */
+    public function testGetY(): void
+    {
+        static::assertSame(90.0, (new EquiRectangularProjection())->getY(new Position(0, 0)));
+        static::assertSame(180.0, (new EquiRectangularProjection())->getY(new Position(0, -90)));
+        static::assertSame(0.0, (new EquiRectangularProjection())->getY(new Position(0, 90)));
+    }
+
+    /**
+     * @covers ::getMaxX
+     */
+    public function testGetMaxX(): void
+    {
+        static::assertSame(360.0, (new EquiRectangularProjection())->getMaxX());
+    }
+
+    /**
+     * @covers ::getMaxY
+     */
+    public function testGetMaxY(): void
+    {
+        static::assertSame(180.0, (new EquiRectangularProjection())->getMaxY());
+    }
+}
