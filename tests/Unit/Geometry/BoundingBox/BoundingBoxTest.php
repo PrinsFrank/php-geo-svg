ca7560ecde394751a80e974b07a17f7bcc52a7f0
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2021-10-31 19:09:57 +0100

    Add unit tests for bounding box

--- tests/Unit/Geometry/BoundingBox/BoundingBoxTest.php
+++ tests/Unit/Geometry/BoundingBox/BoundingBoxTest.php
@@ -8,6 +8,8 @@ use PHPUnit\Framework\TestCase;
 use PrinsFrank\PhpGeoSVG\Exception\InvalidBoundingBoxException;
 use PrinsFrank\PhpGeoSVG\Geometry\BoundingBox\BoundingBox;
 use PrinsFrank\PhpGeoSVG\Geometry\BoundingBox\BoundingBoxPosition;
+use PrinsFrank\PhpGeoSVG\Geometry\Position\Position;
+use PrinsFrank\PhpGeoSVG\Projection\MercatorProjection;
 
 /**
  * @coversDefaultClass \PrinsFrank\PhpGeoSVG\Geometry\BoundingBox\BoundingBox
@@ -72,5 +74,66 @@ class BoundingBoxTest extends TestCase
         $this->expectExceptionMessage('The longitude of the NorthEastern coordinate (-90) is west of the SouthWestern coordinate (90)');
 
         new BoundingBox(new BoundingBoxPosition(90, 0), new BoundingBoxPosition(-90, 0));
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getWidth
+     */
+    public function testGetWidth(): void
+    {
+        static::assertSame(360.0, (new BoundingBox(new BoundingBoxPosition(-180, 0), new BoundingBoxPosition(180, 0)))->getWidth());
+        static::assertSame(90.0, (new BoundingBox(new BoundingBoxPosition(-90, 0), new BoundingBoxPosition(0, 0)))->getWidth());
+        static::assertSame(180.0, (new BoundingBox(new BoundingBoxPosition(-90, 0), new BoundingBoxPosition(90, 0)))->getWidth());
+        static::assertSame(90.0, (new BoundingBox(new BoundingBoxPosition(0, 0), new BoundingBoxPosition(90, 0)))->getWidth());
+        static::assertSame(0.0, (new BoundingBox(new BoundingBoxPosition(90, 0), new BoundingBoxPosition(90, 0)))->getWidth());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getHeight
+     */
+    public function testGetHeight(): void
+    {
+        static::assertSame(0.0, (new BoundingBox(new BoundingBoxPosition(0, 0), new BoundingBoxPosition(0, 0)))->getHeight());
+        static::assertSame(180.0, (new BoundingBox(new BoundingBoxPosition(0, -90), new BoundingBoxPosition(0, 90)))->getHeight());
+        static::assertSame(90.0, (new BoundingBox(new BoundingBoxPosition(0, -90), new BoundingBoxPosition(0, 0)))->getHeight());
+        static::assertSame(90.0, (new BoundingBox(new BoundingBoxPosition(0, 0), new BoundingBoxPosition(0, 90)))->getHeight());
+    }
+
+    /**
+     * @covers ::boundX
+     */
+    public function testBoundX(): void
+    {
+        $position = new Position(90, 45);
+        $projection = $this->createMock(MercatorProjection::class);
+        $projection->expects(static::exactly(3))
+            ->method('getX')
+            ->withConsecutive([$position], [$this->equalTo(new Position(0, 0))], [$this->equalTo(new Position(-180, 0))])
+            ->willReturnOnConsecutiveCalls(30.0, 20.0, 50.0);
+
+        static::assertSame(
+            60.0,
+            (new BoundingBox(new BoundingBoxPosition(0, 0), new BoundingBoxPosition(180, 90)))->boundX($position, $projection)
+        );
+    }
+
+    /**
+     * @covers ::boundY
+     */
+    public function testBoundY(): void
+    {
+        $position = new Position(90, 45);
+        $projection = $this->createMock(MercatorProjection::class);
+        $projection->expects(static::exactly(3))
+            ->method('getY')
+            ->withConsecutive([$position], [$this->equalTo(new Position(0, 90))], [$this->equalTo(new Position(0, 0))])
+            ->willReturnOnConsecutiveCalls(30.0, 20.0, 50.0);
+
+        static::assertSame(
+            60.0,
+            (new BoundingBox(new BoundingBoxPosition(0, 0), new BoundingBoxPosition(180, 90)))->boundY($position, $projection)
+        );
     }
 }
