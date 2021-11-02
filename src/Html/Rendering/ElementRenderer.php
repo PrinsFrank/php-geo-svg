7be9ef2db3ac546c4e816d83b5151b1c21261f13
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2021-11-02 20:33:24 +0100

    Reduce elementRenderer complexity

--- src/Html/Rendering/ElementRenderer.php
+++ src/Html/Rendering/ElementRenderer.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace PrinsFrank\PhpGeoSVG\Html\Rendering;
 
+use PrinsFrank\PhpGeoSVG\Exception\InvalidArgumentException;
 use PrinsFrank\PhpGeoSVG\Exception\RecursionException;
 use PrinsFrank\PhpGeoSVG\Html\Elements\Element;
+use PrinsFrank\PhpGeoSVG\Html\Elements\Text\TextContent;
 
 class ElementRenderer
 {
@@ -21,22 +23,51 @@ class ElementRenderer
             throw new RecursionException('Recursion limit of ' . self::RECURSION_LIMIT . ' Reached');
         }
 
-        $elementContent = null;
-        foreach ($element->getChildElements() as $childElement) {
-            $elementContent .= str_repeat(self::INDENTING_CHAR, $currentDepth) . self::renderElement($childElement, $currentDepth);
-        }
-
-        if (null !== $element->getTextContent()) {
-            $elementContent .= str_repeat(self::INDENTING_CHAR, $currentDepth) . $element->getTextContent()->getContent() . PHP_EOL;
+        $childElementContent = self::renderChildElements($element->getChildElements(), $currentDepth);
+        $textContent = self::renderTextContent($element->getTextContent(), $currentDepth);
+        if (null === $childElementContent && null === $textContent && $element->canSelfClose()) {
+            return self::renderSelfClosingElement($element);
         }
 
         $attributeString = AttributeRenderer::renderAttributes($element->getAttributes());
-        if (null === $elementContent && $element->canSelfClose()) {
-            return '<' . $element->getTagName() . (null !== $attributeString ? ' ' . $attributeString : null) . '/>' . PHP_EOL;
+        return '<' . $element->getTagName() . (null !== $attributeString ? ' ' . $attributeString : null) . '>' . PHP_EOL .
+            $childElementContent . $textContent .
+        str_repeat(self::INDENTING_CHAR, $currentDepth - 1) . '</' . $element->getTagName() . '>' . PHP_EOL;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public static function renderSelfClosingElement(Element $element): string
+    {
+        if ($element->canSelfClose() === false) {
+            throw new InvalidArgumentException('Only elements that can self close can be rendered as self closing elements');
         }
 
-        return '<' . $element->getTagName() . (null !== $attributeString ? ' ' . $attributeString : null) . '>' . PHP_EOL .
-            $elementContent .
-        str_repeat(self::INDENTING_CHAR, $currentDepth - 1) . '</' . $element->getTagName() . '>' . PHP_EOL;
+        $attributeString = AttributeRenderer::renderAttributes($element->getAttributes());
+        return '<' . $element->getTagName() . (null !== $attributeString ? ' ' . $attributeString : null) . '/>' . PHP_EOL;
+    }
+
+    /**
+     * @param Element[] $childElements
+     * @throws RecursionException
+     */
+    public static function renderChildElements(array $childElements, int $currentDepth): ?string
+    {
+        $childElementString = null;
+        foreach ($childElements as $childElement) {
+            $childElementString .= str_repeat(self::INDENTING_CHAR, $currentDepth) . self::renderElement($childElement, $currentDepth);
+        }
+
+        return $childElementString;
+    }
+
+    public static function renderTextContent(?TextContent $textContent, int $currentDepth): ?string
+    {
+        if (null !== $textContent) {
+            return str_repeat(self::INDENTING_CHAR, $currentDepth) . $textContent->getContent() . PHP_EOL;
+        }
+
+        return null;
     }
 }
